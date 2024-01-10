f1866789668ac798302894e7e44949ab0c4f0012
matt11matthew <matt11matthew@gmail.com>
2024-01-09 23:01:13 -0500

    Update NumberUtils.java

--- src/main/java/me/matthewedevelopment/atheriallib/utilities/number/NumberUtils.java
+++ src/main/java/me/matthewedevelopment/atheriallib/utilities/number/NumberUtils.java
@@ -15,7 +15,21 @@ public class NumberUtils {
             return false;
         }
     }
-
+    public static String formatMoney(double number) {
+        if (number < 1000) {
+            return String.format("%.0f", number); // No formatting for numbers less than 1000
+        } else if (number < 1000000) {
+            return String.format("%.1fK", number / 1000); // For thousands
+        } else if (number < 1000000000) {
+            return String.format("%.1fM", number / 1000000); // For millions
+        } else if (number < 1000000000000L) {
+            return String.format("%.1fB", number / 1000000000); // For billions
+        } else if (number < 1000000000000000L) {
+            return String.format("%.1fT", number / 1000000000000L); // For trillions
+        } else {
+            return String.format("%.1fQ", number / 1000000000000000L); // For quadrillions
+        }
+    }
     public static int[] range(int min, int max) {
         List<Integer> integers = new ArrayList<>();
         if (min == max) {
