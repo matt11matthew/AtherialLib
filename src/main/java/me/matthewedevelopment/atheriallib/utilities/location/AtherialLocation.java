518258b656d8f3bc6663512033cdc1ff5863b266
matt11matthew <matt11matthew@gmail.com>
2024-01-29 21:36:34 -0500

    Fixed

--- src/main/java/me/matthewedevelopment/atheriallib/utilities/location/AtherialLocation.java
+++ src/main/java/me/matthewedevelopment/atheriallib/utilities/location/AtherialLocation.java
@@ -33,6 +33,54 @@ public class AtherialLocation {
         this.z = z;
     }
 
+    public String getWorld() {
+        return world;
+    }
+
+    public void setWorld(String world) {
+        this.world = world;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(float yaw) {
+        this.yaw = yaw;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch = pitch;
+    }
+
     public Location toLocation(){
         if (this.world==null) return null;
         return toLocation(null);
