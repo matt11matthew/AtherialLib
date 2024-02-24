065f1db9a8a2ea66a21c72968e7a41e75d5de81d
matt11matthew <matt11matthew@gmail.com>
2024-02-23 20:14:58 -0500

    Update CustomTypeRegistry.java

--- src/main/java/me/matthewedevelopment/atheriallib/config/yaml/CustomTypeRegistry.java
+++ src/main/java/me/matthewedevelopment/atheriallib/config/yaml/CustomTypeRegistry.java
@@ -1,21 +1,19 @@
 package me.matthewedevelopment.atheriallib.config.yaml;
 
-import me.matthewedevelopment.atheriallib.config.yaml.ConfigSerializable;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomTypeRegistry {
 
-    private static final Map<Class<?>, ConfigSerializable<?>> registry = new HashMap<>();
+    private static final Map<String, ConfigSerializable<?>> registry = new HashMap<>();
 
     public static <T> void registerType(Class<T> typeClass, ConfigSerializable<T> serializer) {
-        registry.put(typeClass, serializer);
+        registry.put(typeClass.getSimpleName(), serializer);
         System.out.println("Registered config serializer " + typeClass.getSimpleName());
     }
 
     @SuppressWarnings("unchecked")
     public static <T> ConfigSerializable<T> getSerializer(Class<T> typeClass) {
-        return (ConfigSerializable<T>) registry.get(typeClass);
+        return (ConfigSerializable<T>) registry.get(typeClass.getSimpleName());
     }
 }
