8fbcb2a178fd885712e4052eb27d11099d4bec33
matt11matthew <matt11matthew@gmail.com>
2024-07-23 23:01:52 -0400

    1.21

--- src/main/java/me/matthewedevelopment/atheriallib/nms/Version.java
+++ src/main/java/me/matthewedevelopment/atheriallib/nms/Version.java
@@ -21,7 +21,8 @@ public enum Version {
     V1_16_R1(VersionProviderV1_16_R1.class),
     V1_20_R1(VersionProviderV1_20_R1.class),
     V1_20_R2(VersionProviderV1_20_R2.class),
-    V1_20_R3(VersionProviderV1_20_R3.class);
+    V1_20_R3(VersionProviderV1_20_R3.class),
+    V1_21_R0(VersionProviderV1_21_R0.class);
 
     private Class<? extends VersionProvider> versionProviderClass;
 
