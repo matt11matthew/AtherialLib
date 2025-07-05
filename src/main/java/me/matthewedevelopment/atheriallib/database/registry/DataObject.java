90e7287b6cbb61e4a36d1c522e0b338151a349c3
matt11matthew <matt11matthew@gmail.com>
2025-07-05 14:10:41 -0400

    JSON

--- src/main/java/me/matthewedevelopment/atheriallib/database/registry/DataObject.java
+++ src/main/java/me/matthewedevelopment/atheriallib/database/registry/DataObject.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 public abstract class DataObject<T extends DataObject<T>> {
-    private UUID uuid;
+    protected UUID uuid;
 
 
     public DataObject(UUID uuid) {
